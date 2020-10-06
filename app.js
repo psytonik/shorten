e61917678b111c49183a5b08dea385af7a3ff2b6
Anthony Fink <psytonik@icloud.com>
2020-10-06 09:48:05 +0300

    production fixes

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
 
-const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const path = require('path');
+const cors = require('cors');
 const connectDb = require('./config/db');
 require('dotenv').config();
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 connectDb();
 
 app.use(compression());
 app.use(bodyParser.json());
+
+//// CORS ////
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors({origin: `${process.env.CLIENT_URL}`}));
+}
+
 app.use('/api/auth', require('./route/authRoute'));
 app.use('/api/link', require('./route/linkRoute'));
 app.use('/t', require('./route/redirectRoute'));
